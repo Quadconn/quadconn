0689a9723a0bce840bf78f89f96d8ef4f26ac2f6
Ben Le <benlenet@csu.fullerton.edu>
2026-04-20 22:37:27 +0000

    changed diagnostics to include power

--- src/cpp/motor/motor_interface.cpp
+++ src/cpp/motor/motor_interface.cpp
@@ -39,8 +39,17 @@ int main(int argc, char** argv) {
         moteus::Controller::Options opts{};
         opts.id = def.can_id;
         opts.transport = def.bus;
-        opts.query_format.power = moteus::kFloat;
-        
+        // disable the following replies
+        opts.query_format.mode = moteus::Resolution::kIgnore;
+        opts.query_format.position = moteus::Resolution::kIgnore;
+        opts.query_format.velocity = moteus::Resolution::kIgnore;
+        opts.query_format.temperature = moteus::Resolution::kIgnore;
+        // enable the following replies
+        opts.query_format.voltage = moteus::Resolution::kInt8;
+        opts.query_format.torque = moteus::Resolution::kFloat; 
+        opts.query_format.power = moteus::Resolution::kFloat;  
+        opts.query_format.fault = moteus::Resolution::kInt8;
+    
         // populate BusGroup directly using key-value pairs
         // if already exists, then retrieve reference. Otherwise, create
         // another key-value pair
